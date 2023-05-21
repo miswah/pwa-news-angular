fb81f725568700c709f9e36bebddc62d3f2b3013
miswah <miswah.md@gmail.com>
2023-05-21 13:41:57 +0530

    (enh): news layout set

---
+++ src/app/news-layout/news-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-layout',
+  templateUrl: './news-layout.component.html',
+  styleUrls: ['./news-layout.component.scss']
+})
+export class NewsLayoutComponent {
+
+}
