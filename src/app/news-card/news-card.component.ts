28c997371444cc80ebb702bbbf70b4130c787f58
miswah <miswah.md@gmail.com>
2023-05-21 15:11:00 +0530

    (enh): render http data with cards

--- src/app/news-card/news-card.component.ts
+++ src/app/news-card/news-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { News } from '../helpers/news';
 
 @Component({
   selector: 'app-news-card',
   templateUrl: './news-card.component.html',
-  styleUrls: ['./news-card.component.scss']
+  styleUrls: ['./news-card.component.scss'],
 })
 export class NewsCardComponent {
-
+  @Input() news: News[] = [];
 }
