5f210bb6b4169fd2391c10322abdc992021c0a1d
miswah <miswah.md@gmail.com>
2023-05-21 13:41:38 +0530

    (routing): added for news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"news", component: BaseLayoutComponent}, {path: "", redirectTo: "/news", pathMatch:"full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
