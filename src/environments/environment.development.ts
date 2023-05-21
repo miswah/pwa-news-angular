32a6d547eb6fb609f3c0245a13071df0694b6005
miswah <miswah.md@gmail.com>
2023-05-21 14:34:29 +0530

    (enh): add enviroments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_KEY: 'sxsBuVuIViXTa62lwXk3A6vP1cIhE92WiWSBI01TTIU',
+  API_URL: 'https://api.newscatcherapi.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  API_KEY: 'sxsBuVuIViXTa62lwXk3A6vP1cIhE92WiWSBI01TTIU',
+};
