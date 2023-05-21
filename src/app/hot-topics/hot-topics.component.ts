cd675948886b181a17fec59188ad9bff2012a779
miswah <miswah.md@gmail.com>
2023-05-21 15:10:21 +0530

    (enh): render data from http call

--- src/app/hot-topics/hot-topics.component.ts
+++ src/app/hot-topics/hot-topics.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { News } from '../helpers/news';
 
 @Component({
   selector: 'app-hot-topics',
   templateUrl: './hot-topics.component.html',
-  styleUrls: ['./hot-topics.component.scss']
+  styleUrls: ['./hot-topics.component.scss'],
 })
 export class HotTopicsComponent {
-
+  @Input() news: News[] = [];
 }
