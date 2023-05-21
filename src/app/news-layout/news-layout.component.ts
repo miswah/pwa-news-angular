5eebf2028520de9d67e0ae8ee9e2f74f86453deb
miswah <miswah.md@gmail.com>
2023-05-21 15:10:47 +0530

    (enh): api call added to get news array and pass to child components

--- src/app/news-layout/news-layout.component.ts
+++ src/app/news-layout/news-layout.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsManagementService } from '../services/news-management.service';
+import { map, tap } from 'rxjs';
+import { News } from '../helpers/news';
 
 @Component({
   selector: 'app-news-layout',
   templateUrl: './news-layout.component.html',
-  styleUrls: ['./news-layout.component.scss']
+  styleUrls: ['./news-layout.component.scss'],
 })
-export class NewsLayoutComponent {
+export class NewsLayoutComponent implements OnInit {
+  public latestNews: News[] = [];
+  public news: News[] = [];
+  constructor(private newsManagement: NewsManagementService) {}
 
+  ngOnInit(): void {
+    this.newsManagement
+      .getLatestNews('US', 'business')
+      .pipe(map((x: any) => x.articles))
+      .subscribe((data: News[]) => {
+        this.latestNews = data.slice(0, 5);
+        this.news = data.slice(5, -1);
+      });
+  }
 }
